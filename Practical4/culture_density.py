8b32ff610f3a6482a127aa047b5022dad6c43095
Yuan Y <156878984+RemnantDay@users.noreply.github.com>
2024-03-06 13:29:28 +0800

    Update culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -11,4 +11,8 @@ print("On Day",day,"the cell density goes over 90%.")
 # Repeat while cell density is less than 90%:
 #      add one day
 #      cell density doubles
-# When cell density is more than 90%, output the days.
+#      Check cell density:
+#           if cell density less than 90%:
+#            continue!
+#           if cell density more than 90%:
+#             done!
