e1f7f8ea9fe4742f7955fcbe17686321716fd257
Yuan Y <156878984+RemnantDay@users.noreply.github.com>
2024-03-06 13:15:46 +0800

    Update recursive.py

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -5,5 +5,12 @@ for i in range(0,4):         #Repeat 4 times as we already have the first number
    print(b)                  #print the new variable
    a=b                       #Give the old variable "a" a new number so that when starting the loop again, the new variable "b" can work on this new "a".
 
-   #pseudocode comments
-   
+#pseudocode comments
+# Import the first number (4)
+# output number "4" so that we only need to output 4 numbers later.
+# Repeat:
+#     create a number which is the twice of the original number and add three
+#     output this number
+#     check repeat times:
+#         if less than 4: continue the operation
+#         if 4 numbers get output: DONE!
