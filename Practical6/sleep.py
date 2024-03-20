6ca9b90564302341e21e10b1bc3e53a014ae9596
Yuan Y <156878984+RemnantDay@users.noreply.github.com>
2024-03-20 13:31:24 +0800

    Update sleep.py

--- Practical6/sleep.py
+++ Practical6/sleep.py
@@ -1,13 +1,13 @@
-dic={"sleep":8, "classes":6, "study":3.5, "TV":2, "music":1, "other":24-8-6-3.5-2-1}
-print(dic)
-values=list(dic.values())
-keys=list(dic.keys())
+dic={"sleep":8, "classes":6, "study":3.5, "TV":2, "music":1, "other":24-8-6-3.5-2-1}  # Create a dictionary for different activities
+print(dic)  # Print the dictionary
+values=list(dic.values()) # Get all the values in the dictionary
+keys=list(dic.keys()) # Get all the keys in the dictionary
 import matplotlib.pyplot as plt
 plt.figure()
-plt.pie(values,labels=keys,startangle=90,explode=[0,0,0.1,0,0,0])
+plt.pie(values,labels=keys,startangle=90,explode=[0,0,0.1,0,0,0])  # create the par chart, and the "study" part will be moved out a little bit
 plt.show()
 plt.clf()
 
 ###The following code is used to ask the user to input the activity they want to learn
-activity=str(input("Input the activity you want to know (sleep,classes,study,TV,music,other):"))
-print("You spent",dic[activity],"hours on",activity,"in average.")   # output the result
+activity=str(input("Input the activity you want to know (From 'sleep','classes','study','TV','music','other'):"))
+print("You spent",dic[activity],"hours on",activity,"during an average day.")   # output the results
