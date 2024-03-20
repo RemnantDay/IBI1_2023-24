7927a25ee7d83d70409fd8a412479dd8f4354034
RemnantDay <yuanyizhou999@126.com>
2024-03-20 13:20:58 +0800

    edit city

--- Practical6/city.py
+++ Practical6/city.py
@@ -1,14 +1,26 @@
 city_UK=[0.56,0.62,0.04,9.7]
+city_UK1=[0.56,0.62,0.04,9.7]
 city_CHN=[0.58,8.4,29.9,22.2]
+city_CHN1=[0.58,8.4,29.9,22.2]
 city_UK.sort()  # sort values from smaller to bigger
 city_CHN.sort(reverse=True)  # sort values from bigger to smaller
 print(city_UK)
 print(city_CHN)
-import matplotlib.pyplot as plt
+x=[0,1,2,3]
+x1=['Edinburgh','Glasgow','Stirling','London']
+x2=['Haining','Hangzhou','Shanghai','Beijing']  
+color=['red','yellow','peru','deepskyblue']
+import matplotlib.pylab as plt
 plt.figure()
-plt.boxplot(city_UK,vert=True,showmeans=True,widths=0.1)
-plt.suptitle("The Boxplot of population in the UK",fontsize=15) #设置字体大小为15号字体
+plt.xticks(x, x1)  
+plt.xlabel("city in the UK")
+plt.ylabel("population(millions)")
+plt.title("city size in the UK")
+plt.bar(x1, city_UK1,color=color)
 plt.figure()
-plt.boxplot(city_CHN,vert=False,showmeans=True)
-plt.suptitle("The Boxplot of population in China",fontsize=15)
+plt.xticks(x,x2)
+plt.xlabel("city in China")
+plt.ylabel("population(millions)")
+plt.title("city size in China")
+plt.bar(x2,city_CHN1,color=color)
 plt.show()
