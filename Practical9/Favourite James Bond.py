3395bf6782b5f7161348d94f7a535e9e870bdc21
Yuan Y <156878984+RemnantDay@users.noreply.github.com>
2024-04-10 13:13:25 +0800

    Create Favourite James Bond.py

---
+++ Practical9/Favourite James Bond.py
@@ -0,0 +1,16 @@
+def name_JamesBond(x):
+    y=x+18
+    if 1973<=y<=1986:
+        print("The actor is Roger Moore.")
+    if 1987<=y<=1994:
+        print('The actor is Timothy Dalton.')
+    if 1995<=y<=2005:
+        print('The actor is Pierce Brosnan.')
+    if 2006<=y<=2021:
+        print('The actor is Daniel Craig.')
+    if (y<1973) or (y>2021):
+        print('No actor according to the practical')
+x=int(input("The year you born:"))
+name_JamesBond(x)
+
+# The function is called "name_JamesBond" where you need to input the year you born (an integer)
